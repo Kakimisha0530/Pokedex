0ee8cc1e6f53d1c8f1edb1ca8666f85383d8a92e
aichaDiak <aicha.diakite@outloo.com>
2014-10-26 15:29:41 +0100

    Modification de la classe Dresseur

--- cartes/Dresseur.java
+++ cartes/Dresseur.java
@@ -1,7 +1,37 @@
 package cartes;
 
-public class Dresseur {
-	private int type;
-	private String description;
-	private final String[] 	NAME = {};
+/**
+ * 
+ * @author aicha
+ * Cette classe repr&eacute;sente les cartes de type Dresseur.
+ * Ils en existe trois types qui sont : 
+ * Chaque carte poss&egrave;de un nom, un numero dans la collection
+ */
+public class Dresseur extends Carte{
+	private int type_dresseur;
+	private String actions;
+	private final String[] 	TYPE_NOM = {"","",""};
+	private final String[] 	TYPE_DESC = {"","",""};
+	
+	public Dresseur(int num,int type,String nom) {
+		super(num,nom);
+		this.type_dresseur = type;
+	}
+
+	public void setActions(String actions) {
+		this.actions = actions;
+	}
+	
+	public String getActions() {
+		return actions;
+	}
+	
+	public int getTypeDresseur(){
+		return this.type_dresseur;
+	}
+
+	@Override
+	public String typeDeCarte() {
+		return "Dresseur";
+	}
 }
