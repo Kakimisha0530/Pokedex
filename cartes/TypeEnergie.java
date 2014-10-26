6d8d0035ea222c8c480071b0aeab1758cfdb76e7
aichaDiak <aicha.diakite@outloo.com>
2014-10-26 15:30:43 +0100

    Modification de l'interface TypeEnergie

--- cartes/TypeEnergie.java
+++ cartes/TypeEnergie.java
@@ -6,16 +6,16 @@ package cartes;
  *
  */
 public interface TypeEnergie {
-	public static final int COLORLESS = 0;
-	public static final int GRASS = 1;
-	public static final int FIRE = 2;
-	public static final int WATER = 3;
-	public static final int LIGHTNING = 4;
-	public static final int PSYCHIC = 5;
-	public static final int FIGHTING = 6;
-	public static final int DARKNESS = 7;
+	public static final int INCOLORE = 0;
+	public static final int PLANTE = 1;
+	public static final int FEU = 2;
+	public static final int EAU = 3;
+	public static final int ELECTRIQUE = 4;
+	public static final int PSYCHIQUE = 5;
+	public static final int COMBAT = 6;
+	public static final int OBSCURITE = 7;
 	public static final int METAL = 8;
-	public static final int FAIRY = 9;
+	public static final int FEE = 9;
 	public static final int DRAGON = 10;
 	public static final String[] ENERGY_NAME = {};
 	public static final String[] ENERGY_COLOR = {};
