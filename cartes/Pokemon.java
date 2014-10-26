7fc5bf03f6c3f89c0ebcd8f92482dfbf772f618f
aichaDiak <aicha.diakite@outloo.com>
2014-10-26 15:30:01 +0100

    Modification de la classe Pokemon

--- cartes/Pokemon.java
+++ cartes/Pokemon.java
@@ -1,6 +1,25 @@
 package cartes;
 
-public class Pokemon
+/**
+ * 
+ * @author aicha
+ *
+ */
+public class Pokemon extends Carte
 {
-
+	private int niveau;
+	private int points_de_vie;
+	private int type_energie;
+	private Attaque[] attaques;
+	
+	
+	public Pokemon(int num,String nom) {
+		super(num,nom);
+	}
+	
+	@Override
+	public String typeDeCarte() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
