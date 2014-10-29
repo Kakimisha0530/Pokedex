5181600f497af7c37a7f4644afaff7ae67c44040
aichaDiak <aicha.diakite@outloo.com>
2014-10-29 20:28:35 +0100

    Ajout de la classe Pokedeck

---
+++ cartes/Pokedeck.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cartes;
+
+import sauvegarde.SauvegardeBinaire;
+
+/**
+ * Représentation du deck
+ * @author aicha
+ *
+ */
+public class Pokedeck extends SauvegardeBinaire{
+
+	private static final long serialVersionUID = 1L;
+	
+}
