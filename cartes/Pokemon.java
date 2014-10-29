3d0e120b0810b105e5093059b165446c37a34da3
aichaDiak <aicha.diakite@outloo.com>
2014-10-29 03:27:22 +0100

    Ajout de documentation et de getters et setters

--- cartes/Pokemon.java
+++ cartes/Pokemon.java
@@ -1,5 +1,7 @@
 package cartes;
 
+import java.util.ArrayList;
+
 /**
  * 
  * @author aicha
@@ -10,16 +12,55 @@ public class Pokemon extends Carte
 	private int niveau;
 	private int points_de_vie;
 	private int type_energie;
-	private Attaque[] attaques;
+	private ArrayList<Attaque> attaques;
+	private final String[] 	NIVEAU_NOM = {"Basic","Niveau 1","Niveau 2"};
 	
 	
 	public Pokemon(int num,String nom) {
 		super(num,nom);
+		this.attaques = new ArrayList<>();
 	}
 	
+	
+	
+	public int getPoints_de_vie() {
+		return points_de_vie;
+	}
+
+
+
+	public void setPoints_de_vie(int points_de_vie) {
+		this.points_de_vie = points_de_vie;
+	}
+
+
+
+	public int getNiveau() {
+		return niveau;
+	}
+
+
+
+	public int getType_energie() {
+		return type_energie;
+	}
+
+
+
+	public ArrayList<Attaque> getAttaques() {
+		return attaques;
+	}
+
+
+
+	public String[] getNIVEAU_NOM() {
+		return NIVEAU_NOM;
+	}
+
+
+
 	@Override
 	public String typeDeCarte() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Pokemon";
 	}
 }
