e3aeadbf838e037ddbd115a523bb9e69316e518f
aichaDiak <aicha.diakite@outloo.com>
2014-10-29 04:39:15 +0100

    Ajout de documentation et de getters et setters

--- cartes/Pokemon.java
+++ cartes/Pokemon.java
@@ -20,45 +20,64 @@ public class Pokemon extends Carte
 		super(num,nom);
 		this.attaques = new ArrayList<>();
 	}
-	
-	
-	
-	public int getPoints_de_vie() {
-		return points_de_vie;
-	}
 
-
-
-	public void setPoints_de_vie(int points_de_vie) {
-		this.points_de_vie = points_de_vie;
-	}
-
-
-
+	/**
+	 * Retourne le nombre correspondant au niveau du pok&eacute;mon
+	 * @return 
+	 */
 	public int getNiveau() {
-		return niveau;
+		return this.niveau;
+	}
+	
+	/**
+	 * Retourne le nom correspondant au niveau du pok&eacute;mon
+	 * @return 
+	 */
+	public String getNomNiveau() {
+		return this.NIVEAU_NOM[this.niveau];
 	}
 
+	/**
+	 * Retourne les points de vie du pok&eacute;mon
+	 * @return 
+	 */
+	public int getPoints_de_vie() {
+		return this.points_de_vie;
+	}
 
-
+	/**
+	 * Retourne le nombre repr&eacute;sentant le type d'&eacute;nergie associ&eacute; au pok&eacute;mon
+	 * @return 
+	 */
 	public int getType_energie() {
-		return type_energie;
+		return this.type_energie;
 	}
 
-
-
-	public ArrayList<Attaque> getAttaques() {
-		return attaques;
+	/**
+	 * Retourne l'attaque &agrave; l'index fourni
+	 * @param index
+	 * @return
+	 */
+	public Attaque getAttaque(int index) {
+		return this.attaques.get(index);
+	}
+	
+	/**
+	 * Permet d'ajouter la description d'une attaque &agrave; notre liste d'attaque
+	 * @param at
+	 */
+	public void addAttaque(Attaque at) {
+		this.attaques.add(at);
 	}
 
-
-
-	public String[] getNIVEAU_NOM() {
-		return NIVEAU_NOM;
+	/**
+	 * Retourne le nom correspondant au type d'&eacute;nergie associ&eacute; &agrave; la carte
+	 * @return
+	 */
+	public String getNomTypeEnergie() {
+		return TypeEnergie.ENERGY_NAME[this.type_energie];
 	}
-
-
-
+	
 	@Override
 	public String typeDeCarte() {
 		return "Pokemon";
